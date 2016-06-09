c0ce8b0d24196b53f7e3b0eefa1028d291c7ae75
gfox <garythinlizzy@hotmail.com>
2016-06-09 16:50:41 +0100

    Adding xml files

--- src/main/java/sofascore/DriverSetup.java
+++ src/main/java/sofascore/DriverSetup.java
@@ -32,7 +32,7 @@ public class DriverSetup {
 
     protected void prepareAndroidForAppium() throws MalformedURLException {
 
-        //starts the appium server for once
+        //starts the appium server
         AppiumServiceBuilder serviceBuilder = new AppiumServiceBuilder();
         serviceBuilder.usingAnyFreePort();
         service = AppiumDriverLocalService.buildService(serviceBuilder);
